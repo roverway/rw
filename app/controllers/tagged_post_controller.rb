19a38b6500867ed6977b7077e1ee4da78afd04e1
rover <mymassbox@gmail.com>
2011-03-13 16:57:08 +0800

    css style finished

--- app/controllers/tagged_post_controller.rb
+++ app/controllers/tagged_post_controller.rb
@@ -1,8 +1,11 @@
 class TaggedPostController < ApplicationController
   def index
     @tagged_posts = Article.tagged_with(params[:tag_name])
+    @tags = Article.tag_counts_on(:keywords)
   end
 
+  # this action is useless after i put the tag cloud in the 
+  # left sidebar with a partial 
   def tag_cloud
     @tags = Article.tag_counts_on(:keywords)
   end
